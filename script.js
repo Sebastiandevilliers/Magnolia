15714730b99885d640f82b01eea820be2357108d
Frank <frank.devilliers@yandex.com>
2022-02-04 21:27:30 +0200

    final

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-var endDate = new Date("Feb 14, 2022 12:00:00").getTime
-var timer =setInterval(function(){
-    
-}, 1000);
-let now = new Date().getTime;
-let t = endDate - now;
 
-if(t >=0){
-    let days = Math.floor(t / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((t % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-    let secs = Math.floor((t % (1000 60)) / 1000);
-    
-    document.getElementById("timer-days").innerHTML = days + 
-"<span class='label'>DAY(S)</span>";
+var countDownDate = new Date("Feb 28,2022,19:39:52").getTime();
 
-document.getElementById("timer-hours").innerHTML= ("0" + hours).slice(-2) +
-"<span class='label'>HR(S)</span>";
-
-document.getElementById("timer-mins").innerHTML= ("0" + mins).slice(-2) +
-"<span class='label'>MIN(S)</span>";
-
-document.getElementById("timer-secs").innerHTML= ("0" + secs).slice(-2) +
-"<span class='label'>SEC(S)</span>";
+const element = document.querySelector('#count-down-timer');
+const rt = document.getElementsByTagName("text");
+function paddedFormat(num) {
+    return num < 10 ? "0" + num : num; 
 }
+
+window.onload = setInterval(function(){
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance/(1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    element.innerHTML = days + ":" +  hours + ":" +  minutes + ":" + seconds;
+
+ //   document.getElementById(element).innerHTML = days +  + hours + minutes +  seconds;
+
+    if(distance <0){
+        clearInterval(myfunc);
+        document.getElementById(element).innerHTML = newFunction();
+    }
+}, 1000);
+
+function newFunction() {
+    return "EXPIRED";
+}
+
